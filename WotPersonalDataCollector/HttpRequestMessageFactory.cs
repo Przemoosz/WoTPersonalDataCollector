00dd40c6af80ad95e852b09b64f3b8bf14112850
Przemysław Szewczak <szewczakprzemyslaw1@gmail.com>
2022-07-29 20:20:30 +0200

    Created requestmessageFactory

---
+++ WotPersonalDataCollector/HttpRequestMessageFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace WotPersonalDataCollector
+{
+    internal class HttpRequestMessageFactory: IHttpRequestMessageFactory
+    {
+        public HttpRequestMessage Create(IRequestObject requestObject, string apiUri)
+        {
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, apiUri);
+            requestMessage.Headers.Add("Accept", "application/json");
+            var serializedObject = JsonConvert.SerializeObject(requestObject);
+            requestMessage.Content = new StringContent(serializedObject, Encoding.UTF8, "application/json");
+            return requestMessage;
+        }
+    }
+
+    internal interface IHttpRequestMessageFactory
+    {
+        HttpRequestMessage Create(IRequestObject requestObject, string apiUri)
+    }
+}
---
+++ WotPersonalDataCollector/RequestObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WotPersonalDataCollector
+{
+    internal class RequestObject: IRequestObject
+    {
+        public string application_id { get; set; }
+    }
+
+    internal interface IRequestObject
+    {
+        public string application_id { get; }
+    }
+}
