2d29abac466927d7de53ff24006fd31299edb863
Przemysław Szewczak <szewczakprzemyslaw1@gmail.com>
2022-10-13 11:57:50 +0200

    Version changes

--- WotPersonalDataCollectorWebApp/CosmosDb/Dto/DtoVersion.cs
+++ WotPersonalDataCollectorWebApp/CosmosDb/Dto/DtoVersion.cs
@@ -1,6 +1,6 @@
 ﻿namespace WotPersonalDataCollectorWebApp.CosmosDb.Dto
 {
-    internal class DtoVersion
+    internal class DtoVersion: IEquatable<DtoVersion>
     {
         public int Major { get; init; }
         public int Minor { get; init; }
@@ -11,6 +11,36 @@
             Major = major;
             Minor = minor;
             Patch = patch;
+        }
+
+        public bool Equals(DtoVersion? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Major == other.Major && Minor == other.Minor && Patch == other.Patch;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((DtoVersion)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Major, Minor, Patch);
+        }
+
+        public static bool operator == (DtoVersion first, DtoVersion other)
+        {
+            return first.Equals(other);
+        }
+
+        public static bool operator !=(DtoVersion first, DtoVersion other)
+        {
+            return !first.Equals(other);
         }
     }
 }
--- WotPersonalDataCollectorWebApp/CosmosDb/Dto/DtoVersionValidator.cs
+++ WotPersonalDataCollectorWebApp/CosmosDb/Dto/DtoVersionValidator.cs
@@ -4,17 +4,20 @@
     internal class DtoVersionValidator: IDataVersionValidator
     {
         private readonly IAspConfiguration _aspConfiguration;
+        private readonly IDtoVersionFactory _dtoVersionFactory;
         private readonly ILogger _logger;
 
-        public DtoVersionValidator(IAspConfiguration aspConfiguration, ILogger logger)
+        public DtoVersionValidator(IAspConfiguration aspConfiguration,IDtoVersionFactory dtoVersionFactory, ILogger logger)
         {
             _aspConfiguration = aspConfiguration;
+            _dtoVersionFactory = dtoVersionFactory;
             _logger = logger;
         }
 
         public void EnsureVersionCorrectness(UserPersonalData userPersonalData)
         {
-            var webDtoVersion = _aspConfiguration.WotDtoVersion;
+            var webDtoVersion = _dtoVersionFactory.Create(_aspConfiguration.WotDtoVersion);
+            var cosmosDtoVersion = _dtoVersionFactory.Create(userPersonalData.ClassProperties.DtoVersion);
 
         }
         
@@ -22,5 +25,6 @@
 
     public interface IDataVersionValidator
     {
+        void EnsureVersionCorrectness(UserPersonalData userPersonalData);
     }
 }
