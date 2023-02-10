9c78f5ec231175d7e628600a38773a6389da205d
Przemysław Szewczak <przmyslawszewczak1@gmail.com>
2023-02-10 21:58:20 +0100

    added missing documentation

--- WotPersonalDataCollectorWebApp/Extensions/DbSetExtension.cs
+++ WotPersonalDataCollectorWebApp/Extensions/DbSetExtension.cs
@@ -2,8 +2,17 @@
 {
 	using Microsoft.EntityFrameworkCore;
 
+	/// <summary>
+	/// Extension methods for <see cref="DbSet{TEntity}"/>.
+	/// </summary>
 	public static class DbSetExtension
 	{
+		/// <summary>
+		/// Removes all data from provided <see cref="DbSet{TEntity}"/>.
+		/// </summary>
+		/// <typeparam name="T">Entity type in data base set.</typeparam>
+		/// <param name="dbSet">Database set.</param>
+		/// <returns>A task that represents the asynchronous operation.</returns>
 		public static async Task RemoveAllData<T>(this DbSet<T> dbSet) where T : class
 		{
 			IAsyncEnumerable<T> dbSetAsAsyncEnumerable = dbSet.AsAsyncEnumerable();
